a62543afac19c89bc74f52817ee4171ebdb89a02
Luan Assis <luan.assis05.santos@gmail.com>
2020-11-14 22:04:18 -0300

    Conexao com banco e criacao da view para entidade Vendedor

---
+++ ldxps/src/main/java/com/assis/ldxps/LdxpsApplication.java
@@ -0,0 +1,13 @@
+package com.assis.ldxps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdxpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdxpsApplication.class, args);
+	}
+
+}
---
+++ ldxps/src/test/java/com/assis/ldxps/LdxpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assis.ldxps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdxpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
